fde30ff0aadeb5ca87336036c43f06d949ecde27
Hisham Kader <hisham319@hotmail.com>
2021-02-27 15:22:25 -0500

    added seeds and schema

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE  burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id integer auto_increment not null,
+    burger_name varchar(100),
+    devoured boolean default false,
+    primary key (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO burgers (burger_name)
+VALUES ("Chicken Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Veggie Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Beef Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Justin Bieber Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("The Belieber Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Despacito Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Baby Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("One Less Lonely Girl Burger");
